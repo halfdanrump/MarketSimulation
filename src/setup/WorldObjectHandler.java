bf884778aef592fa22a3f9952a8b95fe911a3d77
halfdan <dalekk@gmail.com>
2013-07-03 08:33:05 +0900

    Added code for group/latency experiment.

--- src/setup/WorldObjectHandler.java
+++ src/setup/WorldObjectHandler.java
@@ -3,6 +3,7 @@ package setup;
 import java.util.ArrayList;
 
 import utilities.StockMarketPair;
+import utilities.Utils;
 
 import log.AgentLogger;
 import log.Logger;
@@ -18,13 +19,33 @@ import agent.HFT;
 import agent.SingleStockMarketMaker;
 
 public class WorldObjectHandler {
-	
+
 	private static ArrayList<Logger> logs = new ArrayList<Logger>();
 
 	public static void createAgents(){
-//		HFT agent = new HFT();	
-		int[] stockIDs = {0}; int[] marketIDs = {0, 1}; int[] latencies = {2, 3}; long minimumSpread = 100;  
-		new SingleStockMarketMaker(stockIDs, marketIDs, latencies, minimumSpread);
+		int nGroups = 2;
+		int nAgentsInGroup = 10;
+		int[] stockIDs = {0}; 
+		int[] marketIDs = {0, 1}; 
+		int[] latencies = new int[marketIDs.length];
+		long minimumSpreadForAllAgents = 100;
+		int[] minimumLatencyInGroup = {1, 100};
+		int[] maximumLatencyInGroup = {10, 200};
+
+		for(int group = 0; group < nGroups; group++) {
+			/*
+			 * Create agents within each group. Each group have different minimum and maximum latencies
+			 */
+			for(int i=0; i<nAgentsInGroup; i++) {
+				for(int j=0; j<marketIDs.length; j++) {
+					latencies[j] = Utils.getRandomUniformInteger(minimumLatencyInGroup[group], maximumLatencyInGroup[group]);
+				}	
+				new SingleStockMarketMaker(stockIDs, marketIDs, latencies, minimumSpreadForAllAgents, group);
+			}
+		}
+		
+//		stockIDs = {0}; marketIDs = {0, 1}; latencies = {20, 30}; minimumSpread = 100;  
+//		new SingleStockMarketMaker(stockIDs, marketIDs, latencies, minimumSpread, group);
 //		int[] stockIDs2 = {0}; int[] marketIDs2 = {0, 1}; int[] latencies2 = {10, 30}; long minimumSpread2 = 100;  
 //		new SingleStockMarketMaker(stockIDs2, marketIDs2, latencies2, minimumSpread2);
 	}
@@ -59,36 +80,39 @@ public class WorldObjectHandler {
 	
 	public static void createObjectLoggers() {
 		
-		World.warningLog = new WorldLogger(Logging.logFolder,"lineLog_worldWarnings", false, Logger.Type.TXT);
+		
+		
+		World.warningLog = new WorldLogger(Logging.logRootFolder,"lineLog_worldWarnings", false, Logger.Type.TXT);
 		logs.add(World.warningLog);
-		World.errorLog = new WorldLogger(Logging.logFolder,"lineLog_errors", false, Logger.Type.TXT);
+		World.errorLog = new WorldLogger(Logging.logRootFolder,"lineLog_errors", false, Logger.Type.TXT);
 		logs.add(World.errorLog);
-		World.eventLog = new WorldLogger(Logging.logFolder,"lineLog_worldEvents", false, Logger.Type.TXT);
+		World.eventLog = new WorldLogger(Logging.logRootFolder,"lineLog_worldEvents", false, Logger.Type.TXT);
 		logs.add(World.eventLog);
-		World.ruleViolationsLog = new WorldLogger(Logging.logFolder,"lineLog_ruleViolations", false, Logger.Type.TXT);
+		World.ruleViolationsLog = new WorldLogger(Logging.logRootFolder,"lineLog_ruleViolations", false, Logger.Type.TXT);
 		logs.add(World.ruleViolationsLog);
-		World.dataLog = new WorldLogger(Logging.logFolder,"columnLog_worldData", true, Logger.Type.CSV);
+		World.dataLog = new WorldLogger(Logging.logRootFolder,"columnLog_worldData", true, Logger.Type.CSV);
 		logs.add(World.dataLog);
 		
 		for(Stock stock:World.getStocks()){
-			stock.roundBasedDatalog = new StockLogger(Logging.logFolder, "columnLog_roundBased", stock, StockLogger.Type.LOG_AFTER_EVERY_ROUND, Logger.Type.CSV);
+			stock.roundBasedDatalog = new StockLogger(Logging.logRootFolder, "columnLog_roundBased", stock, StockLogger.Type.LOG_AFTER_EVERY_ROUND, Logger.Type.CSV);
 			logs.add(stock.roundBasedDatalog);
-			stock.transactionBasedDataLog = new StockLogger(Logging.logFolder, "columnLog_transactionBased", stock, StockLogger.Type.LOG_AFTER_EVERY_TRANSACTION, Logger.Type.CSV);
+			stock.transactionBasedDataLog = new StockLogger(Logging.logRootFolder, "columnLog_transactionBased", stock, StockLogger.Type.LOG_AFTER_EVERY_TRANSACTION, Logger.Type.CSV);
 			logs.add(stock.transactionBasedDataLog);
 		}
 		
 		for(Orderbook orderbook:World.getOrderbooks()){
-			orderbook.orderflowLog = new OrderbookLogger(Logging.logFolder, "lineLog_orderFlow_", orderbook, OrderbookLogger.Type.ORDER_FLOW_LOG, Logger.Type.TXT);
+			orderbook.orderflowLog = new OrderbookLogger(Logging.logRootFolder, "lineLog_orderFlow_", orderbook, OrderbookLogger.Type.ORDER_FLOW_LOG, Logger.Type.TXT);
 			logs.add(orderbook.orderflowLog);
-			orderbook.eventLog = new OrderbookLogger(Logging.logFolder, "linelog_events_", orderbook, OrderbookLogger.Type.EVENT_LOG, Logger.Type.TXT);
+			orderbook.eventLog = new OrderbookLogger(Logging.logRootFolder, "lineLog_events_", orderbook, OrderbookLogger.Type.EVENT_LOG, Logger.Type.TXT);
 			logs.add(orderbook.eventLog);
 		}
 		
 		for(HFT agent:World.getHFTAgents()){
-			agent.eventlog = new AgentLogger(Logging.logFolder, "lineLog", agent, false, Logger.Type.TXT);
+			agent.eventlog = new AgentLogger(Logging.logRootFolder, "lineLog", agent, Logger.Type.TXT, AgentLogger.headerType.NO_HEADER);
 			logs.add(agent.eventlog);
-			agent.datalog = new AgentLogger(Logging.logFolder, "columnLog", agent, true, Logger.Type.CSV);
-			logs.add(agent.datalog);
+			agent.roundDatalog = new AgentLogger(Logging.logRootFolder, "columnLog_roundbased", agent, Logger.Type.CSV, AgentLogger.headerType.ROUND_DATA);
+			logs.add(agent.roundDatalog);
+			agent.tradeLog = new AgentLogger(Logging.logRootFolder, "columnLog_tradelog", agent, Logger.Type.CSV, AgentLogger.headerType.TRADE_DATA);
 		}
 	}
 	
