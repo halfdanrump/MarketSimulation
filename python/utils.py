40aabe4b86a118fc91189f72702f9868e6deee8e
Halfdan Rump <dalekk@gmail.com>
2013-11-07 23:01:56 +0900

    Added utils module

--- python/utils.py
+++ python/utils.py
@@ -1,4 +1,10 @@
+import numpy as np
 from settings import default_parameters as defpar
+from settings import data_types
+
 
 def get_fundamental_after_shock():
 	return defpar['fundamental_initial_value'] + defpar['fundamental_shock_size']
+
+def empty_data_matrix(n_rows = 1):
+	return np.zeros(shape = n_rows, dtype = data_types.items())
