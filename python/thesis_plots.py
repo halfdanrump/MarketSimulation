1efd02e72dcb81e5bccac6c42f3ead493d4f0d3e
Halfdan Rump <dalekk@gmail.com>
2013-12-04 22:52:39 +0900

    Adds code for issue 29

--- python/thesis_plots.py
+++ python/thesis_plots.py
@@ -41,7 +41,7 @@ def plot_issue_26():
 	PCA and Kmeans for dataset 1
 	"""
 	from sklearn.decomposition import PCA
-	from sklearn.cluster import KMeans
+	from sklearn.cluster import KMeans, AffinityPropagation
 	from plotting import make_color_grouped_scatter_plot, make_scatter_plot_for_labelled_data
 	datapath = '/Users/halfdan/Dropbox/Waseda/Research/MarketSimulation/tex/datasets/d1_sameLatDist_ssmm40_sc100/'
 	#par = pandas.read_pickle(datapath + 'pars.pandas')
@@ -64,10 +64,39 @@ def plot_issue_26():
 	filename = figure_save_path + 'issue_26_PCA_b_clusters.png'
 	make_scatter_plot_for_labelled_data(data_frame=df, x_name='d1', y_name='d2', labels=kmeans.labels_, filename=filename, colormap = colormap, legend=True)
 
+	ap = AffinityPropagation()
+	ap.fit(t)
+	filename = figure_save_path + 'issue_26_PCA_c_affinity.png'
+	make_scatter_plot_for_labelled_data(data_frame=df, x_name='d1', y_name='d2', labels=ap.labels_, filename=filename, colormap = colormap, legend=True)
+
+def plot_issue_29():
+	from data_analysis import reduce_npoints_kmeans
+	from sklearn.decomposition import PCA
+	from plotting import make_color_grouped_scatter_plot
+	"""
+	Use KMeans to reduce number of datapoints and then use affinity propagation
+	"""
+	datapath = '/Users/halfdan/Dropbox/Waseda/Research/MarketSimulation/tex/datasets/d1_sameLatDist_ssmm40_sc100/'
+	#par = pandas.read_pickle(datapath + 'pars.pandas')
+	fit = pandas.read_pickle(datapath + 'fits.pandas')
+	fit = fit.drop('gen', 1)
+	points = reduce_npoints_kmeans(fit, 1000)
+	pca = PCA(n_components='mle')
+	trans = pca.fit_transform(points)
+	columns = ['d1', 'd2', 'd3']
+	df = pandas.DataFrame(trans, columns=columns)
+
+	colormap = brewer2mpl.get_map('RdBu', 'diverging', 4, reverse=True)
+	filename = figure_save_path + 'issue_29.png'
+	make_color_grouped_scatter_plot(data_frame=df, x_name='d1', y_name='d2', color_by='d3', filename=filename, colormap=colormap)
+
+
+
 def remake_all_plots():
 	plot_issue_21()
 	plot_issue_26()
+	plot_issue_29()
 
 
 if __name__ == '__main__':
-	plot_issue_26()
+	plot_issue_29()
