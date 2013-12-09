59e98bebe572c0774863ecd0f3d461b4a4ca69a2
Halfdan Rump <dalekk@gmail.com>
2013-12-09 19:18:27 +0900

    Adds function to calculate stats for each Kmeans cluster

--- python/data_analysis.py
+++ python/data_analysis.py
@@ -1,14 +1,42 @@
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from IO import load_all_generations_as_DataFrame
+import IO
 #import matplotlib.pyplot as plt
 from pandas import DataFrame
 import cPickle
 
+dataset_paths = {
+	'd1':'/Users/halfdan/Dropbox/Waseda/Research/MarketSimulation/tex/datasets/merged_data/d1_sameLatDist_ssmm40_sc100/',
+	'd2':'/Users/halfdan/Dropbox/Waseda/Research/MarketSimulation/tex/datasets/merged_data/d2/'
+}
+
 temp_storage = '/Users/halfdan/temp/'
 
-def reduce_npoints_kmeans(dataset, fit, n_datapoints = 1000, load_from_file = False):
-	store_file = temp_storage + dataset + '_kmeans.pkl'
+
+def issue_41(n_clusters, dataset = 'd1'):
+	"""
+	Calculate clusters for K-means and calculate fitness stats for each cluster
+	"""
+	datapath = dataset_paths[dataset]
+	par_data, gen = IO.load_pickled_generation_dataframe(datapath + 'pars.pandas')
+	fit_data, gen = IO.load_pickled_generation_dataframe(datapath + 'fits.pandas')
+	par_trans, pca, components = calculate_pca(par_data, n_components=4)
+
+	kmeans = KMeans(n_clusters=n_clusters, n_jobs=-1, verbose=0)
+	kmeans.fit(par_trans)
+	fit_data['label'] = kmeans.labels_
+	group = fit_data.groupby('label')
+	print "Count with KMmeans\n", group.count()
+	print "mean with KMmeans\n", group.mean()
+	print "std with KMmeans\n", group.std()
+	print "median with KMmeans\n", group.median()
+	
+
+
+
+def reduce_npoints_kmeans(data, dataset_name = 'd1', n_datapoints = 1000, load_from_file = False):
+	store_file = temp_storage + dataset_name + '_kmeans.pkl'
 	
 	if load_from_file:
 		print 'Loading kmeans from file...'
@@ -17,11 +45,9 @@ def reduce_npoints_kmeans(dataset, fit, n_datapoints = 1000, load_from_file = Fa
 	else:
 		print 'Calculating k-means with %s cluster centers...'%n_datapoints
 		kmeans = KMeans(n_clusters = n_datapoints, n_jobs=-1, verbose=1)
-		kmeans.fit(fit)
+		kmeans.fit(data)
 		with open(store_file, 'wb') as fid:
 			cPickle.dump(kmeans, fid)    
-
-
 	return kmeans.cluster_centers_
 
 def run_kmeans(gene_folder, n_clusters):
@@ -44,3 +70,6 @@ def calculate_pca(dataframe, n_components, whiten = False, normalize = True):
 	components = DataFrame(pca.components_, columns = dataframe.columns)	
 	print components
 	return transformed_data, pca, components
+
+if __name__ == "__main__":
+	issue_41(n_clusters=10)
