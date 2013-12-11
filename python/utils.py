6d4ff2c3178d6e28da17c9fd67eb889935c1330b
Halfdan Rump <dalekk@gmail.com>
2013-12-11 22:41:27 +0900

    Adds function to get indices of points in each reducng cluster

--- python/utils.py
+++ python/utils.py
@@ -9,8 +9,13 @@ def get_fundamental_after_shock():
 def empty_data_matrix(n_rows = 1):
 	return np.zeros(shape = n_rows, dtype = fitness_types.items())
 
-
-
+def get_labels_r2o(cluster_assignment_o2r, labels_r):
+	
+	labels_o = list()
+	for c in labels_r: labels_o.append(np.where(cluster_assignment_o2r == c)[0])
+	labels_o = np.concatenate(labels_o)
+	labels_o = np.setdiff1d(range(len(cluster_assignment_o2r)), labels_o)
+	return labels_o
 
 
 def load_test_trade_data(type = 'stable'):
