e8d561171e4e9477fcf060c6856f4c1fb0fb1117
Halfdan Rump <dalekk@gmail.com>
2013-12-28 22:51:52 +0900

    Commit before rewrite of generation loading function

--- python/IO.py
+++ python/IO.py
@@ -39,15 +39,17 @@ def load_trade_log_data(simulation_log_path):
 def store_generation_as_data_matrix(generation_data, generation_number, path):
     all_genes = list()
     all_fitnesses = list()
+    all_ids = list()
     for gene in generation_data:
         all_genes.append(gene['ind'])
         all_fitnesses.append(gene['fit'])
+        if gene.has_key('data_ids'):
+            all_ids.append(gene['data_ids'])
 
     par_dtype = [(par, int) for par in parameters_in_genes] 
     fit_dtype = [(stat, float) for stat in fitness_types.keys()]
     par_matrix = np.zeros(shape = len(all_genes), dtype = par_dtype)
     fit_matrix = np.zeros(shape = len(all_genes), dtype = fit_dtype)
-    
     for i, (gene, fitness) in enumerate(zip(all_genes, all_fitnesses)):
         for par, val in gene.items():
             par_matrix[par][i] = val
@@ -56,9 +58,10 @@ def store_generation_as_data_matrix(generation_data, generation_number, path):
     
     np.savez_compressed(path + 'gen_%s_pars'%generation_number, par_matrix)
     np.savez_compressed(path + 'gen_%s_fit'%generation_number, fit_matrix)
+    if all_ids: np.savez_compressed(path + 'gen_%s_data_ids'%generation_number, all_ids)
 
 
-def load_all_generations_as_DataFrame(folder_name, drop_invalid_individuals=True):
+def load_all_generations_as_DataFrame(folder_name, drop_invalid_individuals=True, with_data_ids = True):
     #gen_par = dict()
     from pandas import concat
     all_par = DataFrame()
@@ -92,9 +95,17 @@ def load_all_generations_as_DataFrame(folder_name, drop_invalid_individuals=True
         all_fit = all_fit.drop(i)
         all_par = all_par.reset_index(drop=True)
         all_fit = all_fit.reset_index(drop=True)
-        return all_par, all_fit, invalid_individuals
+        return all_par, all_fit, all_data_ids, invalid_individuals
     else:
-        return all_par, all_fit, DataFrame()
+        return all_par, all_fit, all_data_ids, DataFrame()
+
+def save_tradeprice_data(rounds, tradePrice, fitness, all_parameters, filename):
+    pars_in_gene = dict([(k, all_parameters[k]) for k in settings.parameters_in_genes])
+    fit = dict(zip(list(fitness.dtype.names), list(fitness.item())))
+    if settings.SAVE_DATA_USED_FOR_PLOTTING:     
+        data = {'rounds':rounds, 'tradePrice':tradePrice, 'fitness': fit, 'parameters':pars_in_gene}
+        np.savez_compressed(filename, data)
+
 
 def load_tradeprice_data(filename):
     """
@@ -116,11 +127,13 @@ def load_tradeprice_data_with_parameters(filename):
     par = d['parameters']
     return rounds, prices, fit, par
 
-def pickle_generation_data(dataset_name):
+def pickle_generation_data(dataset_name, with_data_ids = True):
     dataset_path = '/Users/halfdan/Dropbox/Waseda/Research/MarketSimulation/Thesis/datasets/'
-    par, fit, invalids = load_all_generations_as_DataFrame(dataset_path + 'raw_data/%s/generations/'%dataset_name)
+    par, fit, ids, invalids = load_all_generations_as_DataFrame(dataset_path + 'raw_data/%s/generations/'%dataset_name, with_data_ids)
     par.to_pickle(dataset_paths[dataset_name] + 'pars.pandas')
     fit.to_pickle(dataset_paths[dataset_name] + 'fits.pandas')
+    if with_data_ids: ids.to_pickle(dataset_paths[dataset_name] + 'ids.pandas')
+
 
 def load_pickled_generation_dataframe(dataset_name):
     datapath = dataset_paths[dataset_name]
